8b470cad19d202db35c296e7e1f44aeed015cef0
OhJeUng <bioman3238@gmail.com>
2022-06-24 23:25:16 +0900

    초기 환경 세팅
    Mysql
    RabbitMQ

---
+++ src/main/java/com/limited/sales/LimitedSalesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.limited.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedSalesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitedSalesServiceApplication.class, args);
+	}
+
+}

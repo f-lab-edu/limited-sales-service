f9a50c814bb6ef3b7f2f144a6501928de0ec8f45
OhJeUng <bioman3238@gmail.com>
2022-08-05 15:18:15 +0900

    오타 수정

--- src/main/java/com/limited/sales/utils/JwtProperties.java
+++ src/main/java/com/limited/sales/utils/JwtProperties.java
@@ -7,6 +7,6 @@ public class JwtProperties {
   public static final int REFRESH_EXPIRATION_TIME_MS = 60000 * 60 * 24; // 24 시간
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
-  public static final String USER_EMAIL = "userEmail";
+  public static final String USER_EMAIL = "email";
   public static final String BLACKLIST_POSTFIX = "_black";
 }

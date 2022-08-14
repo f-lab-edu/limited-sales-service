50b460750cd87cd42a5057abb95c3992a9df333e
daramii <maedoong2@gmail.com>
2022-08-14 22:04:04 +0900

    Producer 추가

--- src/main/java/com/limited/sales/product/ProductService.java
+++ src/main/java/com/limited/sales/product/ProductService.java
@@ -3,5 +3,7 @@ package com.limited.sales.product;
 import com.limited.sales.product.vo.Product;
 
 public interface ProductService {
-  void saveProduct(final Product product);
+  void save(final Product product);
+
+  void order(final Product product);
 }

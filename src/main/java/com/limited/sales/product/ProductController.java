14a1b9c8cd4d10fc44d8e9477b13b560dcfce2fc
OhJeUng <bioman3238@gmail.com>
2022-10-08 20:50:30 +0900

    상품 수정, 삭제, 등록 맵핑 uri 추가

--- src/main/java/com/limited/sales/product/ProductController.java
+++ src/main/java/com/limited/sales/product/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Slf4j
 @Validated
@@ -22,12 +23,37 @@ public class ProductController {
   private final ProductService productService;
 
   /**
-   * @param product
+   * 상품 조회
+   *
+   * @param productId
+   * @return
+   */
+  @GetMapping("{id}")
+  public HttpResponse<Product> findProduct(
+      @PathVariable("id") @NotNull(message = "상품 아이디가 존재하지 않습니다.") final Integer productId) {
+    final Product foundProduct = productService.findProduct(productId);
+
+    if (foundProduct == null) {
+      return HttpResponse.toResponse(HttpStatus.BAD_REQUEST, "상품이 존재하지 않습니다.", null);
+    } else {
+      return HttpResponse.toResponse(HttpStatus.OK, "상품 조회 성공", foundProduct);
+    }
+  }
+
+  /**
+   * 상품 전체 조회
+   *
    * @return
    */
   @GetMapping
-  public HttpResponse<Void> findProduct() {
-    return null;
+  public HttpResponse<List<Product>> findProductList() {
+    final List<Product> foundProductList = productService.finalProductsList();
+
+    if (foundProductList == null) {
+      return HttpResponse.toResponse(HttpStatus.BAD_REQUEST, "상품 목록이 존재하지 않습니다.", null);
+    } else {
+      return HttpResponse.toResponse(HttpStatus.OK, "상품 목록 조회 성공", foundProductList);
+    }
   }
 
   /**
@@ -63,6 +89,42 @@ public class ProductController {
       return HttpResponse.toResponse(HttpStatus.OK, "상품이 수정되었습니다.");
     } else {
       return HttpResponse.toResponse(HttpStatus.BAD_REQUEST, "상품을 수정하는데 실패했습니다.");
+    }
+  }
+
+  /**
+   * 수량 변경
+   *
+   * @param productId
+   * @param quantity
+   * @return
+   */
+  @PatchMapping("/{id}")
+  public HttpResponse<Void> updateQuantity(
+      @PathVariable("id") @NotNull(message = "상품 아이디가 존재하지 않습니다.") final Integer productId,
+      @RequestParam @NotNull(message = "상품 수량이 존재하지 않습니다.") final Integer quantity) {
+
+    if (productService.updateQuantity(productId, quantity) > 0) {
+      return HttpResponse.toResponse(HttpStatus.OK, "수량이 변경되었습니다.");
+    } else {
+      return HttpResponse.toResponse(HttpStatus.BAD_REQUEST, "수량 변경을 실패했습니다.");
+    }
+  }
+
+  /**
+   * 상품 삭제
+   *
+   * @param productId
+   * @return
+   */
+  @DeleteMapping("/{id}")
+  public HttpResponse<Void> productDelete(
+      @PathVariable("id") @NotNull(message = "상품 아이디가 존재하지 않습니다.") final Integer productId) {
+
+    if (productService.deleteProduct(productId) > 0) {
+      return HttpResponse.toResponse(HttpStatus.OK, "상품이 삭제되었습니다.");
+    } else {
+      return HttpResponse.toResponse(HttpStatus.BAD_REQUEST, "상품 삭제 실패.");
     }
   }
 }

3791c45255d15569a53a74a8eaf7da98f595aac5
OhJeUng <bioman3238@gmail.com>
2022-10-11 22:32:36 +0900

    Exception 수정, setMessage 추가

--- src/main/java/com/limited/sales/exception/ExceptionVo.java
+++ src/main/java/com/limited/sales/exception/ExceptionVo.java
@@ -2,18 +2,24 @@ package com.limited.sales.exception;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@NoArgsConstructor
 public class ExceptionVo<T> {
-    private final HttpStatus status;
-    private final String message;
-    private final T data;
+    private HttpStatus status;
+    private String message;
+    private T data;
 
     @Builder
     public ExceptionVo(HttpStatus status, String message, T data) {
         this.status = status;
         this.message = message;
         this.data = data;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
